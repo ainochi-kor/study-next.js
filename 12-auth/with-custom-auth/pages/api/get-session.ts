60e3bf3602273aaefee14d741f3bb497eee5863e
ainochi-kor <kthrkdals@gmail.com>
2023-04-13 01:38:40 +0900

    add with-custom-auth: api를 이용하여 로그인 시스템 예시 구축

---
+++ 12-auth/with-custom-auth/pages/api/get-session.ts
@@ -0,0 +1,20 @@
+import { decode } from "@/lib/jwt";
+import { parse } from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const getSession = (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET") {
+    return res.status(404).end();
+  }
+
+  const { my_auth } = parse(req.headers.cookie || "");
+
+  if (!my_auth) {
+    return res.json({ loggedIn: false });
+  }
+
+  return res.json({
+    loggedIn: true,
+    user: decode(my_auth),
+  });
+};
